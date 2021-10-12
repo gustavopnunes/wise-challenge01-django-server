b31c5972e71dcb761f73bc811c77224bb0e05b7a
Gustavo Prado Nunes <gustavo@pop-os.localdomain>
2021-10-12 16:11:34 -0300

    fix product prices model

--- wise_products/models.py
+++ wise_products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=50)
     quantidade = models.IntegerField()
-    preco = models.FloatField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.nome
